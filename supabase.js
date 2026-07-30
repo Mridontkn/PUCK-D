361f3c889d3a9d8673dc76b70e19a3de4d7f6de5
Mridontkn <124101732+Mridontkn@users.noreply.github.com>
2026-07-30 03:13:13 -0600

    ddjd

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://otnnyxtttsiypjskrshw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_cM5C9YCHNKdz8DvjFSJ_PQ_eA-wQ76P";
-
-const db = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
